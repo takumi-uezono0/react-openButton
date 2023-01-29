e4b1c87ee9809d6e48f3d6115ad4fa7abe1dfe7a
t.uezono <103921788+takumi-uezono0@users.noreply.github.com>
2023-01-29 18:25:07 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,21 @@
+import { memo } from "react";
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("レンダリングしました");
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
